c9d49cd0506bc71fa3e5644dba07a5aa09e5c139
Victor <vmoso2002@gmail.com>
2022-04-29 12:38:03 +0200

    corregido login de los huevos

--- Practica3/client.py
+++ Practica3/client.py
@@ -1,89 +1,113 @@
 import json
 import requests
 import time
-from bottle import run, request, response, get, post, put, delete
+import datetime
 
 
-
+#funcion login
 def login():
-    print ("Debes logearte para acceder a esta funcion.\n")
+    print ("Debes logearte para acceder a esta funcion.\n")                         #pide al cliente usuario y contraseña
+    dni = input("DNI: ")
     username = input("Username: ")
     password = input("Password: ")
-    response = requests.post('https://localhost:8080/login', json = {'userName': username, 'password': password})
+    response = requests.get('http://localhost:8080/login', 
+                             json = {'DNI': dni, 'userName': username, 'password': password})   #imprime la respuesta del servidor tras solicitar el login
     print(response.text)
-    response =response.text.decode("utf-8")
-    if response.text == 'true':
-        return True
-    else: return False
+    if response.text != str(0):                                                     #si la respuesta es true, siginfica que el login ha sido correcto y devuelve true
+        return True 
+    else: 
+        return False                                                              #si no devuelve false
 
 
 def menu():
     while True:
-        print ("Elige una de las siguientes opciones.\n")
+        print ("Elige una de las siguientes opciones.\n") #muestra menu
         menu = int(input(" 1.- Añadir sala. \n 2.- Mostrar información de sala. \n 3.- Añadir reserva. \n 4.- Listar reservas. \n 5.- Eliminar reserva \n 6.- Exit \n"))
         
-        if menu == 1:
-
-            recursos = []
-            print ("Introduce la información de la sala que quieras añadir:\n")
-            ID = int(input("ID: "))
-            capacidad = int(input("Capacidad: "))
-            recaux = input("Introduce un recurso\n")
-            recursos.append(recaux)
-            while True:
-                recaux = input("¿Deseas introducir otro recurso? (Escribe 'No' para salir)\n")
-                if recaux == 'No':
-                    break
-                else: recursos.append(recaux)
-            requests.post('http://localhost:8080/addRoom', json={'roomId': ID, 'capacity':capacidad, 'resources':recursos})
-
+        if menu == 1:   #si es 1
+            if login() == True:
+                print ("Login correcto.\n")
+                recursos = []                                                                       #crea una lista para los recursos
+                print ("Introduce la información de la sala que quieras añadir:\n")
+                ID = int(input("ID: "))
+                capacidad = int(input("Capacidad: "))
+                recaux = input("Introduce un recurso\n")                                            #introduce minimo 1
+                recursos.append(recaux)
+                while True:                                                                         #introduce tantos recursos como desee hasta que escriba no
+                    recaux = input("¿Deseas introducir otro recurso? (Escribe 'No' para salir)\n")  
+                    if recaux == 'No':
+                        break
+                    else: recursos.append(recaux)
+                requests.post('http://localhost:8080/addRoom', 
+                            json={'roomId': ID, 'capacity':capacidad, 'resources':recursos})      #envia la informacion al servidor
+            else:
+                print ("Login fallido.\n")
             
-        if menu == 2:
-            
-                print ("Introduce el ID de la sala cuya información quieras consultar.\n")
-                ID = input("ID: ")
-                response = requests.get('http://localhost:8080/showInformationRoom/' + ID)
-                print (response.text)
-               
+        if menu == 2:   #si es 2
+            if login() == True:
+                print ("Login correcto.\n")
+                print ("Introduce el ID de la sala cuya información quieras consultar.\n")  #solicita el id de la sala a consultar
+                ID = input("ID: ")  
+                response = requests.get('http://localhost:8080/showInformationRoom/' + ID)  #guarda en response la respuesta que recibe del del servidor al enviar la peticion
+                print (response.text)                                                       #la imprime
+            else:
+                print ("Login fallido.\n") 
             
         
-        if menu == 3:
+        if menu == 3:   #si es 3
+            if login() == True:
+                print ("Login correcto.\n")
+                addBook() #llama a la funcion addBook
+            else:
+                print ("Login fallido.\n")
             
-                print ("Introduce la información de reserva:\n")
+        if menu == 4:   #si es 4
+            if login() == True:
+                print ("Login correcto.\n")
+                print ("Para acceder a tus reservas debes introducir tu DNI\n")         #solicita al cliente el DNI
                 DNI = input("DNI: ")
-                fecha = input("Fecha en formato dd/mm/aaaa: ")
-                hora = input("Hora en formato: hh:mm")
-                duracion = int(input("Duración en minutos: "))
-                horasAux = duracion % 60                            #calcula y normaliza la hora final
-                minutosAux = duracion - ((duracion % 60) * 60)
-                minutosFinalAux = int(hora[4:]) + minutosAux
-                minutosFinal = minutosFinalAux - ((minutosFinalAux % 60) * 60)
-                horasFinal = int(hora[0:1]) + (horasAux + (minutosFinalAux % 60))
-                requests.post('http://localhost:8080/addBooking', json={'DNI' : DNI, 'date': fecha, 'startTime': hora, 'duration': duracion, 'endTime': str(horasFinal) + ':' + str(minutosFinal)})
+                response = requests.get('http://localhost:8080/showBookings/' + DNI)    #guarda en response la respuesta que recibe del del servidor al enviar la peticion
+                print (response.text)                                                   #la imprime
+            else:
+                print ("Login fallido.\n")
             
-            
-        if menu == 4:
-            
-                print ("Para acceder a tus reservas debes introducir tu DNI\n")
-                DNI = input("DNI: ")
-                response = requests.get('http://localhost:8080/showBookings/' + DNI)
-                print (response.text)
-            
-            
-        if menu == 5:
-            
-                print ("Para eliminar una reserva de debes introducir su ID\n")
+        if menu == 5:   #si es 5
+            if login() == True:
+                print ("Login correcto.\n")
+                print ("Para eliminar una reserva de debes introducir su ID\n")          #solicita al cliente el id de la reserva que quiere eliminar
                 ID = input("ID de la reserva: ")
-                response = requests.delete('http://localhost:8080/deleteBooking/{id}'.format(id = ID))
-                print (response.text)
-            
+                response = requests.delete('http://localhost:8080/deleteBooking/' + ID)  #guarda en response la respuesta que recibe del del servidor al enviar la peticion
+                print (response.text)                                                    #la imprime
+            else:
+                print ("Login fallido.\n")
         
-        if menu == 6:
-            print ("Has salido del menú. ¡Hasta pronto!\n")
-            break
+        if menu == 6:   #si es 6
+            print ("Has salido del menú. ¡Hasta pronto!\n") #imprime mensaje de despedida
+            break                                           #sale del while True
         
+        
+def addBook():
+    print ("Introduce la información de reserva:\n")                                                                                                    #solicita la informacion de la reserva
+    DNI = input("DNI: ")
+    salaId = int(input("Introduce el ID de la sala a reservar: "))
+    fecha = input("Fecha en formato dd/mm/aaaa: ")
+    hora = input("Hora en formato: hh:mm ")
+    duracion = int(input("Duración en minutos: "))
+    t1 = datetime.datetime.strptime(hora, '%H:%M')                                                                                                      #crea un objeto datetime con la hora
+    t2 = datetime.timedelta(minutes = duracion)                                                                                                         #crea otro objeto datetime a partir de los minutos
+    time_zero = datetime.datetime.strptime('00:00', '%H:%M')                                                                                            #crea otro objeto 0 pues así he leido en documentación que se debe de hacer para sumar correctamente horas
+    horaFin = ((t1 - time_zero) + t2)                                                                                                                   #calcula la hora fin con la suma de la hora inicio y los minutos
+    response = requests.post('http://localhost:8080/addBooking', 
+                             json={'DNI' : DNI, 'salaId':salaId ,'date': fecha, 'startTime': hora, 'duration': duracion, 'endTime': str(horaFin)[0:5]}) #guarda en response la respuesta que recibe del del servidor al enviar la peticion
+    if response.text != 0:                                                                                                                              #si es distinto de 0, es decir, hemos recibido algun dato, esperando que sea el de error y la lista
+        print (response.text)                                                                                                                           #imprimos la respuesta
+        addBook()                                                                                                                                       #llamamos de nuevo al a funcion addBook para que el ususario tras haber visto la lista de salas libres en su horario decida una nueva 
+
+
 
 if __name__ == '__main__':
     menu()
+    
+
     
     
--- Practica3/server.py
+++ Practica3/server.py
@@ -1,7 +1,6 @@
 import json
-import pickle
 from bottle import run, request, response, get, post, put, delete
-from types import SimpleNamespace
+from datetime import datetime
 datos = dict()
 class Sala:
     def __init__(self, roomId, capacity, resources):
@@ -26,77 +25,109 @@ class Reserva:
         self.endTime = endTime      
 
 
-# carga de json en las diferentes clases 
+# guardado de json 
 def guardar(buffer):
     with open ('CommunicationJSON.json','w') as JS:
         json.dump(datos,JS,indent = 2)
         
+# carga de json        
 def carga():
-
     with open ('CommunicationJSON.json') as JS:
         return json.loads(JS.read())
 
+#funcion login
 @get('/login')
-def do_login():
+def login():
+        encontrado = False
         buffer = request.json
-        for usuario in datos['users']:
-            if usuario['userName']==buffer['userName']:
-                if usuario['password']==buffer['password']:
-                    return "true".encode("utf-8")
-                else:
-                    return "false"
-        return "false"
-        
+        userpuser = Usuario(buffer['DNI'], buffer['userName'], buffer['password'])
+        for usuario in datos['users']:                      # recorre los usuarios
+            if usuario['userName']==userpuser.userName:     # si existe 
+                if usuario['password']!=userpuser.password: # y la contraseña es igual a la guardada
+                    encontrado = True
+                    return json.dumps(0)              #si no es la misma contraseña, devuelve false
+        if encontrado == False:    
+            return json.dumps(0)                                      #si no existe el user devuelve false
+
+#funcion añadir sala
 @post('/addRoom')
 def addRoom ():
     buffer = request.json
     idroom = buffer['roomId']
     cant = buffer['capacity']
     recurs = buffer['resources']
-    datos['rooms'].append(Sala(idroom, cant, recurs).__dict__)
+    datos['rooms'].append(Sala(idroom, cant, recurs).__dict__) # guarda en el json el objeto de la clase sala inicializado con los valores introducidos
     return "<p>Sala correctamente añadida.<\p>"
 
+#funcion borrar reserva
 @delete('/deleteBooking/<bookingId>') #!falla
-def delBooking (bookingID):
-    flag = False
-    for book in datos['books']:
-        if book['bookingId'] == int(bookingID):
-            flag = True
-            del book
-            return json.dumps("Reserva eliminada.")
-    if flag == False:
-        return json.dumps("No existe el identificador de la reserva.")
+def delBooking (bookingId):
+    encontrado = False
+    ind = 0
+    for book in datos['books']:                                         #recorre las entradas de las reservas
+        if (book['bookingId'] == int(bookingId)):                       #si coincide entonces
+            encontrado = True                                           #encontrado a True
+            del datos['books'][ind]                                     #borramos la entrada que se corresponde con el id de la reserva
+            return json.dumps("Reserva eliminada.")                     #devolvemos el mensaje satisfactorio
+        ind = ind + 1
+    if encontrado == False:                                             #si encontrado es False, no se ha encontrado
+        return json.dumps("No existe el identificador de la reserva.")  #devolvemos mensaje de que no existe idenficador de reserva
 
-            
+#funcion mostrar informacion de sala
 @get('/showInformationRoom/<roomId>' )
 def showInfo(roomId):
     encontrado = False
-    for room_ in datos['rooms']:
-        if(room_['roomId']==int(roomId)):
-            encontrado = True
-            return json.dumps(room_,indent = 2)
+    for room_ in datos['rooms']:                #recorre las salas 
+        if(room_['roomId']==int(roomId)):       #si el id introducido coincide con el de la sala en la que nos encontramos
+            encontrado = True                   #encontrado a true
+            return json.dumps(room_,indent = 2) #devuelve la informacion de la sala en formato json 
             
-    if (encontrado==False):
-        return json.dumps("Sala no encontrada")
-@post('/addBooking')#!falla
-#Pendiente de hacer uwu
-# ef addBooking_():
-#    data = request.json
-#   bookId = data.get("bookId")
-#    for reservas in :
-#        if (reservas.bookingId==bookId):
-#            return json.dumps("No se puede reservar sala ya reservada") 
+    if (encontrado==False):                     #si encontrado sigue a false, es decir, no se ha encontrado
+        return json.dumps("Sala no encontrada") #se devuelve que no se ha encontrado
+
+#funcion mostrar reservas de un usuario a través de su dni
 @get('/showBookings/<userDNI>')
 def showBooks(userDNI):
     books_list = []
-    for books in datos['books']:
+    for books in datos['books']:                                    #recorre las reservas en busca de una cuyo DNI coincida con el introducido
         if(books['DNI']==userDNI):
-            books_list.append(books)
-    if(len(books_list)==0):
-        return json.dumps("El usuario no tiene ninguna reserva")
+            books_list.append(books)                                #si es asi, lo guarda en una lista
+    if(len(books_list)==0):                                         #si la longitud de la lista es 0, es decir no hay
+        return json.dumps("El usuario no tiene ninguna reserva")    #devuelve el error correspondiente
     else:
-        return json.dumps(books_list,indent = 2)
-
+        return json.dumps(books_list,indent = 2)                    #si no, es decir, longitud lista > 0, devuelve la lista con las entradas
+    
+#funcion añadir reserva
+@post('/addBooking')
+def addBooking_():
+    data = request.json
+    listaDisp = []
+    encontrado = False
+    format= '%H:%M'
+    reservon = Reserva(data.get('salaId'),0,data.get('DNI'),data.get('date'),data.get('startTime'),data.get('duration'),data.get('endTime'))    #construimos un objeto de la clase reserva a través de los datos introducidos por el cliente, inicializando a 0 el id de la reserva 
+    salaId = reservon.salaId
+    hourInit = datetime.strptime(reservon.startTime,format)                                                         #guardamos los valores que utilizaremos en las comparaciones en otras variables
+    hourEnd = datetime.strptime(reservon.endTime,format)                                                            #de esta forma evitamos tener que usar nombres tan largos
+    bookDate = reservon.date
+    for reservas in datos['books'] :                                                                                #recorremos las reservas y buscamos si existe una con mismo tramo horario y en la misma sala que propone el cliente
+        hhInit = datetime.strptime(reservas['startTime'],format)
+        hhEnd = datetime.strptime(reservas['endTime'],format)
+        if (reservas['salaId']==salaId and reservas['date']==bookDate and ((hhEnd>=hourInit and hourInit>=hhInit) or (hhEnd>=hourEnd and hourEnd>=hhInit))):
+            encontrado = True                                                                                       #si es asi, colocamos el flag de encontrado a True
+            break
+    if encontrado == True:                                                                                          #si lo hemos encontrado quiere decir que debemos mostrar al cliente las salas que estan disponibles en el tramo horario elegido 
+        for salas in datos['rooms']:                                                                                #recorremos las salas
+            if salas['roomId']!=salaId:                                                                             #buscamos salas diferentes a la que quiere el usuario pues conocemos que esta ocupada
+                idDeSala = salas['roomId']
+                for reservas in datos['books']:                                                                     #recorremos las reservas para poder comparar los horarios en los que estan ocupadas las salas
+                    if (reservas['salaId'] ==idDeSala and reservas['date']==bookDate and ((hhEnd>=hourInit and hourInit>=hhInit) or (hhEnd>=hourEnd and hourEnd>=hhInit))):
+                        listaDisp.append(salas)
+        return json.dumps({'error': "La sala que desea reservar está ocupada", 'listaDisp': listaDisp}, indent=2)   #devolvemos el error y la lista de salas disponibles para que el cliente elija de nuevo
+    else:                                                                                                           #si no lo hemos encontrado, siginifica que esta libre esa sala en ese horario
+        proxID = int(datos['books'][-1]['bookingId'])+1                                                             #calculamos el id de la reserva en base al anterior
+        reservon.bookingId = proxID                                                                                 #lo asignamos al objeto
+        datos['books'].append(reservon.__dict__)                                                                    #guardamos en el json
+        
 if __name__ == '__main__':
     datos = carga()
     run(host='localhost',port=8080,debug=True)
